9caffb93827cadd05452a24d35ebed8e2dc61d7e
Mathieu Couture <mathieu.couture@inspq.qc.ca>
2018-04-27 16:05:50 -0400

    Ajout d'un seed script

---
+++ setup.groovy
@@ -0,0 +1,93 @@
+import groovy.transform.Field
+
+@Field final String REPERTOIRE_RACINE = 'utilitaire-nam'
+
+@Field final String PIPELINE_CONSTRUCTION = "${REPERTOIRE_RACINE}/utilitaire-nam-construction"
+@Field final String PIPELINE_DEPLOIEMENT = "${REPERTOIRE_RACINE}/utilitaire-nam-deploiement"
+@Field final String PIPELINE_LIVRAISON = "${REPERTOIRE_RACINE}/utilitaire-nam-livraison"
+@Field final String PIPELINE_TAG = "${REPERTOIRE_RACINE}/utilitaire-nam-etiquetage"
+
+folder("${REPERTOIRE_RACINE}") {
+    description ("Utilitaire NAM")
+}
+
+pipelineJob("${PIPELINE_CONSTRUCTION}") {
+    description ('Construction de utilitaire-nam')
+    definition {
+        cpsScm {
+            scm {
+                git{
+                	remote{
+              			url('https://gitlab.forge.gouv.qc.ca/inspq/utilitaire-nam.git')
+          			}
+                	branch ('origin/master')
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
+// pipelineJob("${PIPELINE_TAG}") {
+//     description ('Étiquetage (tag) de SADU Panorama')
+//     parameters {
+//         stringParam('VERSION_TAG', '', 'Numéro de version a assigner au Tag de SADU')
+//         stringParam('VERSION_NEXT','', 'Numéro a assigner à la prochaine version de SADU (sans SNAPSHOT)')
+//         stringParam ('MESSAGE', 'Nouveau tag ${VERSION_TAG} par Jenkins', 'Numéro a assigner à la prochaine version de SADU (sans SNAPSHOT)')
+//     }
+//     definition {
+//         cpsScm {
+//             scm {
+//                 git{
+//                 	remote{
+//               			url('https://gitlab.forge.gouv.qc.ca/sipmi/sadu-panorama.git')
+//           			}
+//                 	branch ('origin/master')
+//                 }
+//             }
+//             scriptPath('tag.Jenkinsfile')
+//         }
+//     }
+// }
+
+// pipelineJob("${PIPELINE_DEPLOIEMENT}") {
+//     description ('Déploiement de SADU Panorama dans un environnement')
+//     parameters {
+//     	gitParam('TAG'){
+//     	    description('Version de SADU à déployer')
+//     	    type('BRANCH_TAG')
+//     	    tagFilter('*')
+//     	    sortMode('DESCENDING_SMART')
+//     	}
+//         stringParam('ENV', '', 'Environnement sur lequel on déploie SADU')
+//     }
+//     definition {
+//         cpsScm {
+//             scm {
+//                 git {
+//                 	remote {
+//                 	    url('https://gitlab.forge.gouv.qc.ca/sipmi/sadu-panorama.git')
+//                 	}
+//                     branch ('${TAG}')
+//                 }
+//             }
+            
+//             scriptPath('deploy.Jenkinsfile')
+//         }
+//     }
+// }
+// pipelineJob("${PIPELINE_LIVRAISON}") {
+//     description ('Livraison de SADU Panorama')
+//     definition {
+//         cpsScm {
+//             scm {
+//                 git{
+//                 	remote{
+//               			url('https://gitlab.forge.gouv.qc.ca/sipmi/sadu-panorama.git')
+//           			}
+//                 	branch ('origin/master')
+//                 }
+//             }
+//             scriptPath('delivery.Jenkinsfile')
+//         }
+//     }
+// }

ecb3eacebbddbc80bd07ed14f22133c63e579d32
Philippe Gauthier <philippe.gauthier@inspq.qc.ca>
2019-10-28 11:53:54 -0400

    FA-3512 Mettre le port de remote debug dans l'inventaire LOCAL.

--- utilitaire-NAM-Service/docker-entrypoint.sh
+++ utilitaire-NAM-Service/docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [ "$DEBUG_PORT"  ];then
-	if [[ $DEBUG_PORT -ne 0 ]]; then 
-		DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=$DEBUG_PORT,server=y,suspend=n"
-	fi
+if [[ "$DEBUG_PORT" != "0" ]]; then
+	DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=$DEBUG_PORT,server=y,suspend=n"
 fi
 
-exec $JAVA_HOME/bin/java $DEBUG -Dvaadin.productionMode=true -jar $UNAM_HOME/utilitaire-nam-service.jar
+exec $JAVA_HOME/bin/java $DEBUG -jar $UNAM_HOME/utilitaire-nam-service.jar
 exit $?
